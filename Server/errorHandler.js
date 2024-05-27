cd52e4f2aa0d0df4b105efa32278ba7bfe765ab9
Justin <jpdrenka@gmail.com>
2024-05-27 04:01:53 -0700

    Added navbar to error handler UI

--- Server/errorHandler.js
+++ Server/errorHandler.js
@@ -11,10 +11,14 @@ module.exports = (err, req, res, next) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Error</title>
             <link rel="stylesheet" href="/styles.css">
+            <link rel='stylesheet' href='/loginform.css'>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jersey+25&family=Lobster&family=Lobster+Two:ital@1&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
             <style>
                 body {
                     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-                    display: flex;
+                    display: block;
                     justify-content: center;
                     align-items: center;
                     height: 100vh;
@@ -66,6 +70,13 @@ module.exports = (err, req, res, next) => {
             </style>
         </head>
         <body>
+
+            <div class='navbar' id = 'login_navbar'>
+                <a class='smallatag' href='/sensorview'>
+                <h1 class = 'jersey-25-regular' id = 'login_logo'> <span class = 'withinlogo'>e</span>S</h1>
+                </a>
+            </div>
+                <br>
             <div class="error-container">
                 <div class="emoji">⚙️</div>
                 <h1>Server Error</h1>
